1a8a94b100d7c5016d433fdf84cb02fefcdf9c74
Vikkydssv <38271419+Vikkydssv@users.noreply.github.com>
2022-05-22 18:44:31 +0530

    Added dynamic handling for footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
 })
 export class FooterComponent {
-
   categoriesData: string[][] = [
-    ["Graphics","PSDs","Icons","Templates","Themese",],
-    ["Addons","Fonts","Textures","Patterns","Backgrounds",],
-    ["Buttons","Form","Gui","Navigation","UI kits",],
-    ["Infographics","Graphs","Logos","Vectors","Business Cards",]
+    ["Graphics", "PSDs", "Icons", "Templates", "Themese"],
+    ["Addons", "Fonts", "Textures", "Patterns", "Backgrounds"],
+    ["Buttons", "Form", "Gui", "Navigation", "UI kits"],
+    ["Infographics", "Graphs", "Logos", "Vectors", "Business Cards"],
   ];
 
+  copyRightLinks: string[] = [
+    "Affiliates",
+    "Contributors",
+    "Press / Media",
+    "Careers",
+    "About",
+    "Contact",
+  ];
+
+  copyRightIcons: string[] = [
+    "fa-brands fa-facebook icon",
+    "fa-brands fa-twitter-square icon",
+    "fa-brands fa-google-plus-g icon",
+    "fa-solid fa-rss icon",
+  ];
 }
